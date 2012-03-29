9e4f53bf9e7f30ff7e1651a8e6d3273a278b7895
drewcallan <drewcallan1@gmail.com>
2012-03-29 14:29:16 -0700

    What I did: 1.) added a text Label ("Music Stat App"). 2.) added a (temp) graphic with music and charts/graphs. 3.) added a few more options to the 'Help' drop down menu (and separated there 'def' in the code). 4.) added a directory search button/function. 5.) lastly, just redid the layout a little bit.

--- main_interface.py
+++ main_interface.py
@@ -1,40 +1,61 @@
 from Tkinter import *
-import tkMessageBox
+import tkMessageBox, Tkconstants, tkFileDialog
 
 def callback():
-	print "called the callback"
+	print "Call from Callback"
+def about():
+	print "About the Program"
+def team():
+	print "Team Members"
+def version():
+	print "Current Version: 1.0.1"
 
-def warning(title, text):
-	tkMessageBox.showwarning(title, text)
+def info(title, text):
+	tkMessageBox.showinfo(title, text)
 
 class App:
 	def __init__(self, master):
 		frame = Frame(master, width=600, height=400)
-		frame.pack()
+		frame.pack(padx=10)
+		
+		photo = PhotoImage(file="\project\images\logo.gif")
+		lPhoto = Label(frame, image=photo)
+		lPhoto.image=photo
+		lPhoto.pack(side=TOP)
 
+		
 		#Label(master, text="Directory: ").grid(row=0)
-
-		self.button = Button(frame, text="Quit", fg="red", bg="PeachPuff", command=frame.quit)
-		self.button.pack(side=LEFT)
-
-		self.hello = Button(frame, text="Hello", command=self.hi)
-		self.hello.pack(side=LEFT)
-
-		self.new = Button(frame, text="New", command=self.new)
-		self.new.pack(side=LEFT)
-
+		tText       = Label (frame, text="\n Music Stat App!\n").pack(side=TOP)
+		self.new    = Button(frame, text="New", padx=35, pady=2, command=self.new).pack(side=LEFT)
+		self.hello  = Button(frame, text="Hello", padx=30, pady=2, command=self.hi).pack(side=LEFT)
+		self.askdir = Button(frame, text="Select Directory", padx=10, pady=2, command=self.askdir).pack(side=LEFT)
+		self.button = Button(frame, text="Quit", padx=30, pady=2, fg="red", bg="PeachPuff", command=frame.quit).pack(side=LEFT)
+		bText       = Label (frame, text="\n\n").pack(side=BOTTOM)
+		
 		#self.warn = Button(frame, text="Warn", command=warning("Sample", "Sample Warning"))
 		#self.warn.pack(side=LEFT)
-
-	def hi(self):
-		print "Hello, World"
-
+		
+		self.dir_opt = options = {}
+		options['initialdir'] = 'C:\\'
+		options['mustexist'] = False
+		options['parent'] = root
+		options['title'] = 'Select your music folder'
+		
 	def new(self):
 		top = Toplevel()
 		top.title("Popup")
 		top.button = Button(top, text="Exit", command=top.destroy)
 		top.button.pack()
+		
+	def hi(self):
+		print "Hello, World"
 
+	def askdir(self):
+		"""Returns a selected directoryname."""
+		dirName = tkFileDialog.askdirectory(**self.dir_opt)
+		print dirName
+		return dirName
+		
 root = Tk()
 
 app = App(root)
@@ -51,6 +72,8 @@ filemenu.add_command(label="Exit", command=root.quit)
 
 helpmenu = Menu(menu)
 menu.add_cascade(label="Help", menu=helpmenu)
-helpmenu.add_command(label="About...", command=callback)
+helpmenu.add_command(label="About...", command=about)
+helpmenu.add_command(label="Team...", command=team)
+helpmenu.add_command(label="Version...", command=version)
 
 root.mainloop()
